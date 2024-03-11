1267d392fd915cbfd080b91277c81644b0dd0fde
hiroky1983 <hirockysan1983@gmail.com>
2024-03-11 23:30:52 +0900

    更新

--- main.go
+++ main.go
@@ -1,86 +1,72 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"runtime/trace"
+	"sync"
 	"time"
 )
 
 // goの並列処理とchannelの学習
 func main() {
-	fmt.Println("init")
-	times := []int{1, 2, 3, 4, 5}
-	for _, t := range times {
-		go func() {
-			fmt.Println(t)
-		}()
-	}
-	go func () {
-		fmt.Println("goroutine起動")
-	}()
-	fmt.Println("終了します")
-	time.Sleep(1 * time.Second)
-	fmt.Println("終了")
-
-	// s1()
-	s2()
-	time.Sleep(2 * time.Second)
+	learnGoRoutine()
+	learnTracer()
 }
 
-// result 
-// init
-// 終了します
-// goroutine起動
-// 5
-// 2
-// 3
-// 4
-// 1
-// 終了
-// chに2を送信: 1
-// fmt.Printf("chに1を送信: %v\n", <-ch) が実行される前にch <- 1が実行されているため、deadlockが発生している為
-func s1() {
-	ch := make(chan int)
+func learnGoRoutine() {
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
-		ch <- 1
-		fmt.Printf("chに1を送信: %v\n", <-ch)
+		defer wg.Done()
+		fmt.Println("goroutine invoked")
 	}()
-	fmt.Printf("chに2を送信: %v\n", <-ch)
-
+	wg.Wait()
+	fmt.Printf("num of goroutine: %d\n", runtime.NumGoroutine())
+	fmt.Println("main function finished")
 }
 
-// result 
-// init
-// 終了します
-// goroutine起動
-// 4
-// 5
-// 3
-// 1
-// 2
-// 終了
-// fatal error: all goroutines are asleep - deadlock!
-
-// goroutine 1 [chan send]:
-// main.s2()
-// 	/Users/yamadahiroki/myspace/goroutine-test/main.go:57 +0x77
-// main.main()
-// 	/Users/yamadahiroki/myspace/goroutine-test/main.go:25 +0x185
-
-// goroutine 17 [chan send]:
-// main.s2.func1()
-// 	/Users/yamadahiroki/myspace/goroutine-test/main.go:54 +0x27
-// created by main.s2 in goroutine 1
-// 	/Users/yamadahiroki/myspace/goroutine-test/main.go:53 +0x66
-// exit status 2
-// ch <- 2が実行される前にch <- 1が実行されているため、deadlockが発生している為
-func s2() {
-	ch := make(chan int)
-	go func() {
-		ch <- 1
-		fmt.Printf("chに1を送信: %v\n", <-ch)
-	}()
-	ch <- 2
-	fmt.Printf("chに2を送信: %v\n", <-ch)
+func learnTracer() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	if err := trace.Start(f); err != nil {
+		log.Fatal(err)
+	}
+	defer trace.Stop()
+	ctx, t := trace.NewTask(context.Background(), "main")
+	defer t.End()
+	fmt.Printf("The number of logical CPU Cores: %d\n", runtime.NumCPU())
+	// task(ctx, "task1")
+	// task(ctx, "task2")
+	// task(ctx, "task3")
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go cTask(ctx, &wg, "task1")
+	go cTask(ctx, &wg, "task2")
+	go cTask(ctx, &wg, "task3")
+	wg.Wait()
+	fmt.Println("main function finished")
+}
 
+func task(ctx context.Context, name string) {
+	defer trace.StartRegion(ctx, name).End()
+	time.Sleep(time.Second)
+	fmt.Println(name)
+}
 
+func cTask(ctx context.Context,wg *sync.WaitGroup, name string) {
+	defer trace.StartRegion(ctx, name).End()
+	defer wg.Done()
+	time.Sleep(time.Second)
+	fmt.Println(name)
+}
