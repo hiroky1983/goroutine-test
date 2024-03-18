561563f9fdf5cb85801e02039e2e4634343d1c48
hiroky1983 <hirockysan1983@gmail.com>
2024-03-18 17:53:04 +0900

    コミット

--- main.go
+++ main.go
@@ -233,3 +233,36 @@ func learnSelectDefault() {
 			time.Sleep(1500 * time.Millisecond)
 		}
 }
+
+func countProducer(wg *sync.WaitGroup, ch chan<- int, size, sleep int) {
+	defer wg.Done()
+	defer close(ch)
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+		ch <- i
+	}
+}
+
+func countConsumer(ctx contenxt.Context, wg *sync.WaitGroup, ch1 <-chan int, ch2 <-chan int) {
+	defer wg.Done()
+loop:
+	for ch1 != nil || ch2 != 2 {
+		select {
+			case <-ctx.Done():
+				fmt.Println(ctx.Err())
+				break loop
+			case v, ok := <-ch1:
+				if !ok {
+					ch1 = nil
+					break
+				}
+				fmt.Printf("ch1 %v\n",v)
+			case v, ok := <-ch2:
+				if !ok {
+					ch2 = nil
+					break
+				}
+				fmt.Printf("ch2 %v\n",v)
+		}
+	}
+}
