5429ceb5710e2594d7414dc94758a4ed1b52741b
hiroky1983 <hirockysan1983@gmail.com>
2024-03-22 23:06:12 +0900

    context

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module goroutin
 
 go 1.22.0
 
-require go.uber.org/goleak v1.3.0
+require (
+	go.uber.org/goleak v1.3.0
+	golang.org/x/net v0.22.0
+)
--- main.go
+++ main.go
@@ -27,7 +27,9 @@ func main() {
 	// noMutex()
 	// mutex()
 	// rwMutex()
-	atomicfunc()
+	// atomicfunc()
+	// learningContext()
+	learningContext2()
 }
 
 func learnGoRoutine() {
@@ -399,4 +401,82 @@ func atomicfunc() {
 	wg.Wait()
 	fmt.Println(c)
 	fmt.Println("finished")
+}
+
+func learningContext() {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 400*time.Millisecond)
+	defer cancel()
+	wg.Add(3)
+	go subTask(ctx, &wg, "a")
+	go subTask(ctx, &wg, "b")
+	go subTask(ctx, &wg, "c")
+	wg.Wait()
+
+}
+
+func subTask(ctx context.Context, wg *sync.WaitGroup, id string) {
+	defer wg.Done()
+	t := time.NewTicker(500 * time.Millisecond)
+	select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		case <-t.C:
+			t.Stop()
+			fmt.Println(id)
+		}
+}
+
+func learningContext2() {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 400*time.Millisecond)
+	defer cancel()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		v, err := criticalTask(ctx)
+		if err != nil {
+			fmt.Printf("critical task cancelled due to %v\n", err)
+			cancel()
+			return
+		}
+		fmt.Println("success", v)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		v, err := normalTask(ctx)
+		if err != nil {
+			fmt.Printf("normal task cancelled due to %v\n", err)
+			cancel()
+			return
+		}
+		fmt.Println("success", v)
+	}()
+	wg.Wait()
+}
+
+func criticalTask(ctx context.Context) (string, error) {
+  ctx, cancel := context.WithTimeout(ctx, 800 * time.Millisecond)
+	t := time.NewTicker(1000 * time.Millisecond)
+	defer cancel()
+	select {
+		case <-ctx.Done():
+			return "", ctx.Err()
+		case <-t.C:
+			t.Stop()
+		}
+		return "A", nil
+}
+
+func normalTask(ctx context.Context) (string, error) {
+	t := time.NewTicker(3000 * time.Millisecond)
+	select {
+		case <-ctx.Done():
+			return "", ctx.Err()
+		case <-t.C:
+			t.Stop()
+	}
+	return "B", nil
 }
